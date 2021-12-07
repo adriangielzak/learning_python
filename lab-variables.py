bc586a28039aa6deefc65ca24193e12beec8d8e8
Adrian Giełzak <adrian.gielzak@outlook.com>
2021-12-07 20:12:01 +0100

    Lab - variables

---
+++ lab-variables.py
@@ -0,0 +1,24 @@
+box = 'cola:9 bottles:1 liter'
+print(box)
+
+boxUpper = box.upper()
+print(boxUpper)
+
+boxLower = box.lower()
+print(boxLower)
+
+boxSplit = box.split(':')
+print(boxSplit)
+
+'''
+    W Tym zadaniu liczymy pole kola
+'''
+wartoscPi = 3.14
+promienOkregu = 5
+
+poleKola = wartoscPi * promienOkregu ** 2
+
+print('\n\n', 'Pole kola =', poleKola)
+'''
+    Koniec zadania, w którym liczyliśmy pole kola
+'''
