0d6f3195f2a909bd31911dc9ddee2c0f104b1a65
Adrian Giełzak <adrian.gielzak@outlook.com>
2021-12-07 20:42:58 +0100

    Lab - print

---
+++ lab-print.py
@@ -0,0 +1,34 @@
+print('FR')
+print('FR', 'USA', 'ESP', 'BE')
+france = 'FR'
+spain = 'ESP'
+print(france, 'USA', spain, 'BE')
+pi = 3.14
+r = 10
+print(pi, r)
+print(pi, r, pi * r * r)
+print(pi, r, pi * r ** 2)
+print('area od cycle with radius', r, '=', pi * r ** 2)
+
+print(1, 2, 3)
+print(1, 2, 3, sep='-')
+print(france, 'USA', spain, 'BE', sep='\n')
+print(france, 'USA', spain, 'BE', sep='\t')
+
+print('this is bells: \a\a\a\a\a')
+print('\u03A3')
+print('this is backslash: \\')
+
+'''
+    exercises
+'''
+
+print('TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV')
+print('TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV', sep='; ')
+print('I like computers ', 'TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV', sep=' but better is ')
+
+programName = 'BBC'
+item = 'News'
+time = '18:00'
+print('I like watching', item, 'at', time, 'on', programName, '.')
+print('I like watching ', item, ' at ', time, ' on ', programName, '.', sep='')
